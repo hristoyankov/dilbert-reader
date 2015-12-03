f6919e86f18052ba8a827b9e581751d74bd90442
Hristo Yankov <ico.yankov@gmail.com>
2015-12-04 01:44:40 +0200

    Display today's Dilbert strip.

--- dilbert-reader.js
+++ dilbert-reader.js
@@ -1,20 +1,35 @@
 if (Meteor.isClient) {
-  Template.strip.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.strip.onRendered(function () {
+        Meteor.call('get_dilbert_strip', function(err, response) {
+            if (err)
+                console.log(err)
+            else {
+                Session.set('last_dilbert_strip', response);
+                console.log(response);
+            }
+        });
+    });
+    Template.strip.helpers({
+        dilbert_strip_url: function () {
+            return Session.get('last_dilbert_strip').img_url;
+        },
+        dilbert_strip_title: function () {
+            return Session.get('last_dilbert_strip').title;
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        Meteor.methods({
+            get_dilbert_strip: function () {
+                console.log('In get_dilbert_strip');
+                var data = Scrape.website("http://www.dilbert.com/strip/2015-12-04");
+
+                return {title: data.title, img_url: data.image};
+            }
+        });
+
+        
+    });
 }
