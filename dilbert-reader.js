fc7adb98a16f7c2a7ed03e1d2300a8a01777f307
Hristo Yankov <ico.yankov@gmail.com>
2015-12-05 14:00:26 +0200

    Pseudo random button added.

--- dilbert-reader.js
+++ dilbert-reader.js
@@ -1,8 +1,8 @@
 if (Meteor.isClient) {
     Template.strip.onRendered(function () {
-        Meteor.call('get_dilbert_strip', function(err, response) {
+        Meteor.call('get_dilbert_strip', 'http://dilbert.com/strip/2015-12-04', function(err, response) {
             if (err)
-                console.log(err)
+                console.log(err);
             else {
                 Session.set('last_dilbert_strip', response);
                 console.log(response);
@@ -17,14 +17,25 @@ if (Meteor.isClient) {
             return Session.get('last_dilbert_strip').title;
         }
     });
+
+    Template.strip.events({
+        "click #randomDilbert": function() {
+            Meteor.call('get_dilbert_strip', 'http://dilbert.com/strip/2015-11-04', function(err, response) {
+                if (err)
+                    console.log(err);
+                else
+                    Session.set('last_dilbert_strip', response);
+            });
+        }
+    });
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
         Meteor.methods({
-            get_dilbert_strip: function () {
+            get_dilbert_strip: function (url) {
                 console.log('In get_dilbert_strip');
-                var data = Scrape.website("http://www.dilbert.com/strip/2015-12-04");
+                var data = Scrape.website(url);
 
                 return {title: data.title, img_url: data.image};
             }
